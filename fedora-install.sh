b09a28a8cbe4572c2966c229ca0901bac1e587b1
tarneaux <tarneo@tarneo.fr>
2022-11-15 20:23:46 +0100

    add echos in fedora-install

--- fedora-install.sh
+++ fedora-install.sh
@@ -1,6 +1,11 @@
+echo "updading system"
 sudo dnf update -y
+
+echo "installing packages"
 sudo dnf install awesome dmenu kitty neovim conky git fish stow exa ripgrep trash-cli bat dejavu-fonts-all Xorg xinit mesa-dri-drivers util-linux-user -y
 
+echo "installing blexmono nerd font"
 curl https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/IBMPlexMono/Mono/complete/Blex%20Mono%20Nerd%20Font%20Complete.ttf | sudo tee /usr/share/fonts/Blex\ Mono\ Nerd\ Font\ Complete.ttf > /dev/null
 
+echo "changing shell to fish"
 chsh -s /usr/bin/fish
