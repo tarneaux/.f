677de5fe4793ec442f7392de27f893134d7edd29
tarneaux <tarneo@tarneo.fr>
2022-11-15 20:17:53 +0100

    add missing kitty dependency
    kitty was crashing because the opengl version was too old.

--- fedora-install.sh
+++ fedora-install.sh
@@ -1,4 +1,4 @@
 sudo dnf update -y
-sudo dnf install awesome dmenu kitty neovim conky git fish stow exa ripgrep trash-cli bat dejavu-fonts-all Xorg xinit -y
+sudo dnf install awesome dmenu kitty neovim conky git fish stow exa ripgrep trash-cli bat dejavu-fonts-all Xorg xinit mesa-dri-drivers -y
 
 curl https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/IBMPlexMono/Mono/complete/Blex%20Mono%20Nerd%20Font%20Complete.ttf | sudo tee /usr/share/fonts/Blex\ Mono\ Nerd\ Font\ Complete.ttf > /dev/null
