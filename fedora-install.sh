52c8548b14abbf6af2489ad98273746c1afbe767
tarneaux <tarneo@tarneo.fr>
2022-11-15 20:21:45 +0100

    fix chsh command not found

--- fedora-install.sh
+++ fedora-install.sh
@@ -1,4 +1,6 @@
 sudo dnf update -y
-sudo dnf install awesome dmenu kitty neovim conky git fish stow exa ripgrep trash-cli bat dejavu-fonts-all Xorg xinit mesa-dri-drivers -y
+sudo dnf install awesome dmenu kitty neovim conky git fish stow exa ripgrep trash-cli bat dejavu-fonts-all Xorg xinit mesa-dri-drivers util-linux-user -y
 
 curl https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/IBMPlexMono/Mono/complete/Blex%20Mono%20Nerd%20Font%20Complete.ttf | sudo tee /usr/share/fonts/Blex\ Mono\ Nerd\ Font\ Complete.ttf > /dev/null
+
+chsh -s /usr/bin/fish
