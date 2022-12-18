8595a04c3fb61a5d0131c35fdc62b92e8e67fbf1
tarneaux <tarneo@tarneo.fr>
2022-12-18 13:00:08 +0100

    Refactor config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,20 +7,25 @@ function fish_prompt
 end
 
 
-alias rm="trash"
 alias vim="nvim"
 alias v="nvim"
+
 alias ls="exa --icons --group-directories-first"
 alias ll="exa --icons -l --group-directories-first"
 alias lt='exa -T --icons --group-directories-first'
 alias l="ls"
+
 alias cat="bat"
+
 alias icat="kitty +kitten icat"
-alias s="swallow"
-alias cp="cp --interactive"
+
+alias rm="trash"
+
+alias cp="cp -i"
 alias mv='mv -i'
-alias mkdirs='mkdir -p'
-alias woman="man"
+
+alias md="mkdir"
+alias mds='mkdir -p'
 
 alias ga="git add"
 alias gc="git commit"
@@ -33,25 +38,20 @@ alias gl="git log --decorate --oneline --graph"
 alias gd="git diff"
 
 
-# the terminal rickroll
-alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
-
 # Colorize grep output (good for log files)
 alias grep='grep --color=auto'
-alias egrep='egrep --color=auto'
-alias fgrep='fgrep --color=auto'
+
 
 set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -Ux EDITOR nvim
-set PAGER bat
+set -Ux PAGER bat
 
 
 set PATH ~/.local/share/gem/ruby/3.0.0/bin/ ~/.local/share/CEdev/bin/ ~/.local/bin/ /home/max/.cargo/bin/ ~/.emacs.d/bin/ ~/.nix-profile/bin/ $PATH
 
 
-
+# remove the default fish greeting for a more minimal prompt
 function fish_greeting
-#    fortune -s | python3 ~/.local/bin/box.py
 end
 
 
