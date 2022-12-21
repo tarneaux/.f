7249fea0bfdfd68dbb16bf31f362bc0fab28a42c
tarneaux <tarneo@tarneo.fr>
2022-12-21 17:09:43 +0100

    Prevent ejecting /dev/cdrom by using an uuid for the wifi driver

--- awesome/.config/scripts/wifi_driver.sh
+++ awesome/.config/scripts/wifi_driver.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/bash
-cd /tmp/
-git clone https://github.com/McMCCRU/rtl8188gu.git
-cd rtl8188gu
-make
-sudo make install
-eject /dev/cdrom
+eject /dev/disk/by-label/20180918_154309
+if [ $? -eq 0 ]; then
+    cd /tmp/
+    git clone https://github.com/McMCCRU/rtl8188gu.git
+    cd rtl8188gu
+    make
+    sudo make install
+else
+    echo "that's not connected"
+fi
