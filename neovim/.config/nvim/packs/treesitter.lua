95d256f89e8516a56d86c3afe78d61b51c91a53a
tarneaux <tarneo@tarneo.fr>
2022-12-26 11:48:01 +0100

    Add javascript and astro to treesitter

--- neovim/.config/nvim/packs/treesitter.lua
+++ neovim/.config/nvim/packs/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "rust", "python"},
+    ensure_installed = {"c", "lua", "rust", "python", "javascript", "astro"},
     highlight = {
         enable = true
     }
