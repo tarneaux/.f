3494a841e8561887571124c3b26a834021e666d9
tarneaux <tarneo@tarneo.fr>
2022-12-28 11:12:26 +0100

    Add md to treesitter

--- neovim/.config/nvim/packs/treesitter.lua
+++ neovim/.config/nvim/packs/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "rust", "python", "javascript", "astro"},
+    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown"},
     highlight = {
         enable = true
     }
