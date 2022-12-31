24dd8e0339b01cf7792869f9680dcd8c0a52bcf5
tarneaux <tarneo@tarneo.fr>
2022-12-31 21:23:27 +0100

    add yaml to treesitter

--- neovim/.config/nvim/packs/treesitter.lua
+++ neovim/.config/nvim/packs/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown"},
+    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown", "yaml"},
     highlight = {
         enable = true
     }
