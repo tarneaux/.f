b1fd67b8be3e2a708057816ac939710d8ef0be4d
tarneaux <tarneo@tarneo.fr>
2023-02-01 13:15:58 +0100

    Reorganize config.fish and make one-letter aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,27 +2,33 @@ if [ (tty) = "/dev/tty1" ]
     startx
 end
 
-fish_vi_key_bindings
-
 alias vim="nvim"
-alias v="nvim"
-
-alias ls="exa --icons --group-directories-first"
-alias ll="exa --icons -l --group-directories-first"
-alias lt='exa -T --icons --group-directories-first'
-alias l="ls"
-
-alias cat="bat"
-
-alias rm="trash"
 
 alias cp="cp -i"
 alias mv='mv -i'
 
-alias md="mkdir"
-alias mds='mkdir -p'
-
+alias ls="exa --icons --group-directories-first"
+alias ll="exa --icons -l --group-directories-first"
+alias tree='exa --tree --icons --group-directories-first'
 alias find="echo 'Use fd instead of find' && /bin/find"
+
+alias rm="echo 'Removing forever'; /bin/rm -i"
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+
+# One letter commands for the lazy
+alias v="vim"
+alias t='tree'
+alias l="ls"
+alias c="cd"
+alias m="mkdir"
+alias mp="mkdir -p"
+alias t="trash"
+alias r="rm"
+alias b="bat"
+alias f="fd"
+alias g="git"
 
 alias ga="git add"
 alias gap="git add -p"
@@ -36,9 +42,6 @@ alias gl="git log --decorate --oneline --graph"
 alias gd="git diff"
 alias gb="git branch"
 alias gco="git checkout"
-
-# Colorize grep output (good for log files)
-alias grep='grep --color=auto'
 
 
 set -Ux MANPAGER "sh -c 'col -bx | bat -l man -p'"
@@ -75,7 +78,6 @@ set -gx NODE_REPL_HISTORY $XDG_DATA_HOME/node_repl_history
 set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
 alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
 alias wget="wget --hsts-file=$XDG_DATA_HOME/wget-hsts"
-
 
 starship init fish | source
 
