9d9add7d667fa849d517b5cfe00457b76924373f
tarneaux <tarneo@tarneo.fr>
2023-02-25 15:28:30 +0100

    Add tile left layout to awesome

--- awesome/.config/awesome/screens.lua
+++ awesome/.config/awesome/screens.lua
@@ -30,7 +30,7 @@ screen.connect_signal("property::geometry", set_wallpaper)
 awful.layout.layouts = {
     -- awful.layout.suit.floating,
     awful.layout.suit.tile,
-    -- awful.layout.suit.tile.left,
+    awful.layout.suit.tile.left,
     -- awful.layout.suit.tile.bottom,
     -- awful.layout.suit.tile.top,
     -- awful.layout.suit.fair,
