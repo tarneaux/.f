f0a1aac931efa360572c21269908c1535011c442
tarneaux <tarneo@tarneo.fr>
2023-03-10 16:44:56 +0100

    Update neovim config to support current versions
    (versions of packages)

--- neovim/.config/nvim/packs/lsp.lua
+++ neovim/.config/nvim/packs/lsp.lua
@@ -16,7 +16,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 
 
 -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'bashls', 'html', 'cssls', 'jsonls', 'astro', 'rust_analyzer', 'sumneko_lua' }
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'bashls', 'html', 'cssls', 'jsonls', 'astro', 'rust_analyzer', 'lua_ls' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
--- neovim/.config/nvim/packs/treesitter.lua
+++ neovim/.config/nvim/packs/treesitter.lua
@@ -1,8 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown", "yaml"},
-    highlight = {
-        enable = true
-    }
+    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown", "yaml"}
 }
 
 require'nvim-treesitter.configs'.setup {
