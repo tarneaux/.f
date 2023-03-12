cae49cd0bca4dfe6825976e76a71bd51377acfb8
tarneaux <tarneo@tarneo.fr>
2023-03-12 11:51:14 +0100

    Re-enable treesitter highlighting.
    The treesitter highlighter was crashing because I still had the old
    configurations in ~/.local/share/nvim. After removing those everything
    worked again.

--- neovim/.config/nvim/packs/treesitter.lua
+++ neovim/.config/nvim/packs/treesitter.lua
@@ -1,5 +1,8 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown", "yaml"}
+    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown", "yaml"},
+    highlight = {
+        enable = true,
+    },
 }
 
 require'nvim-treesitter.configs'.setup {
