eac87cd4e905c0708f694b37d19233940c68aec8
tarneaux <tarneo@tarneo.fr>
2023-03-29 20:17:04 +0200

    Add nix treesitter parser to neovim

--- neovim/.config/nvim/packs/treesitter.lua
+++ neovim/.config/nvim/packs/treesitter.lua
@@ -1,7 +1,7 @@
 require('orgmode').setup_ts_grammar()
 
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown", "yaml", "org"},
+    ensure_installed = {"c", "lua", "rust", "python", "javascript", "markdown", "yaml", "org", "nix"},
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = {'org'},
