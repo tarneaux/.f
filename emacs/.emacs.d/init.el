0dcbac44d77f5f788c65c48ef74b7754ac379ead
tarneaux <tarneo@tarneo.fr>
2023-04-03 14:50:11 +0200

    Emacs configs are starting to look good

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,10 @@
+;; keybinding to reload emacs config
+(defun reload-emacs-config ()
+  (interactive)
+  (load-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "C-c r") 'reload-emacs-config)
+
 ;; Disable UI elements
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -11,6 +18,14 @@
 
 ;; Enable automatic bracket insertion by pairs.
 (electric-pair-mode 1)
+
+;; Set the font
+(set-face-attribute 'default nil :font "BlexMono Nerd Font Mono" :height 90)
+
+;; relative line numbers
+(global-display-line-numbers-mode t)
+(setq display-line-numbers 'relative)
+
 
 (require 'package)
 
@@ -53,17 +68,21 @@
   :config (load-theme 'gruvbox-dark-medium t))
 
 
+;; LSP
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-l")
+  :hook ((python-mode . lsp)
+         (rust-mode . lsp)
+         (org-mode . lsp)
+         ;; which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
 
-;; Install and configure company
-(use-package company
-  :config
-  (global-company-mode)
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1))
-
-;; Install and configure flycheck
-(use-package flycheck
-  :config (global-flycheck-mode))
+;; which-key
+(use-package which-key
+  :config (which-key-mode))
 
 ;; Install and configure projectile
 (use-package projectile
@@ -74,7 +93,7 @@
   :config
   (setq org-startup-indented t)
   (setq org-hide-leading-stars t)
-  (setq org-ellipsis " ▾")
+  (setq org-ellipsis " ")
   (setq org-src-fontify-natively t)
   (setq org-src-tab-acts-natively t)
   (setq org-src-preserve-indentation t)
@@ -85,15 +104,26 @@
         '((sequence "TODO" "LATER" "DONE")))
   ;; Open files folded
   (setq org-startup-folded t)
+  (setq org-hide-emphasis-markers t)
 )
 
+;; Install and configure org-superstar
+(use-package org-superstar
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1)))
+  (setq org-superstar-headline-bullets-list '("◉" "○" "✸" "✿")))
+
+
+;; org-evil: shortcuts like >> to demote headings
+(use-package org-evil
+  :config (add-hook 'org-mode-hook 'org-evil-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(package-selected-packages '(which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
