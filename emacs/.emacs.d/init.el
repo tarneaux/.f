20d56a843157e54e52addbaf40eae176e2f9ef22
tarneaux <tarneo@tarneo.fr>
2023-04-04 15:11:03 +0200

    Rewrite emacs config (again)
    Squashed commit of the following:
    
    commit 462eb3980f5808e688195caf8f873c17b9403f77
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Tue Apr 4 15:05:32 2023 +0200
    
        Add final emacs literate config
    
    commit 46a672d2d56453a4cdb01bb3874a0342872efd06
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Tue Apr 4 14:27:21 2023 +0200
    
        Add org-evil
    
    commit 1fda85560c1a807f8ba62fa6bff43ce514857492
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Tue Apr 4 14:26:26 2023 +0200
    
        Add orgmode
    
    commit 28c0fff29a1c822177cdfaf9dd58765da852dc27
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Tue Apr 4 14:17:25 2023 +0200
    
        Remove old emacs configs
    
    commit 8e9cb2612fff0251cff51986e65d9bdde37ebfa6
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Tue Apr 4 14:15:58 2023 +0200
    
        Basic working config
    
    commit f243315055747dac4bc91bd8e3258a9f4c640f03
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Mon Apr 3 19:01:34 2023 +0200
    
        around about right org configs
    
    commit 4af37c17f98ef676d18293f9dff65fe27b912778
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Mon Apr 3 18:04:30 2023 +0200
    
        Add some things
    
    commit 5f6a329214b7027bdf28444b682f5bc670b34990
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Mon Apr 3 17:38:27 2023 +0200
    
        Basic org config
    
    commit d21e5c19308281c81884f57da71e734c5f8637dd
    Author: tarneaux <tarneo@tarneo.fr>
    Date:   Mon Apr 3 16:52:46 2023 +0200
    
        Remove old config

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,124 @@
+(defun reload-config ()
+  (interactive)
+  (org-babel-tangle-file "~/.emacs.d/config.org")
+  (load-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "C-c r") 'reload-config)
+
+(setq default-directory "~/")
+
+;; Prevent emacs from creating lockfiles because they are just annoying
+(setq create-lockfiles nil)
+
+;; Store a limited number of backup files in ~/.local/share/emacs/backups
+(unless (file-exists-p "~/.local/share/emacs/backups")
+  (make-directory "~/.local/share/emacs/backups" t))
+(setq backup-directory-alist '(("." . "~/.local/share/emacs/backups")))
+(setq backup-by-copying t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      version-control t)
+
+;; Store auto-save files in ~/.local/share/emacs/autosaves
+(unless (file-exists-p "~/.local/share/emacs/autosaves")
+  (make-directory "~/.local/share/emacs/autosaves" t))
+(setq auto-save-file-name-transforms
+      `((".*" "~/.local/share/emacs/autosaves/\\1" t)))
+
+;; Store undo history in ~/.local/share/emacs/undo
+(unless (file-exists-p "~/.local/share/emacs/undo")
+  (make-directory "~/.local/share/emacs/undo" t))
+(setq undo-tree-history-directory-alist '(("." . "~/.local/share/emacs/undo")))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(setq package-enable-at-startup nil)
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+(set-face-attribute 'default nil :font "BlexMono Nerd Font Mono-9")
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+
+(use-package gruvbox-theme
+  :config
+  (load-theme 'gruvbox-dark-medium t)
+  ;; I don't like the default line number colors
+  (set-face-attribute 'line-number nil :background "#0000" :foreground "#504945")
+  (set-face-attribute 'line-number-current-line nil :background "#0000")
+  ;; Disable highlighting of new lines
+  (set-face-attribute 'fringe nil :background "#0000"))
+
+(use-package powerline
+  :config
+  (powerline-default-theme))
+
+(use-package evil
+  :config
+  (evil-mode 1))
+
+(use-package evil-terminal-cursor-changer
+  :config
+  (evil-terminal-cursor-changer-activate))
+
+(use-package org
+   :config
+   (setq org-starttup-indented t
+         org-hide-leading-stars t
+         org-ellipsis " ▾"
+         org-src-fontify-natively t
+         org-src-tab-acts-natively t
+         org-src-preserve-indentation t
+         org-confirm-babel-evaluate nil
+         org-src-window-setup 'current-window
+         org-log-done 'time
+         org-todo-keywords '((sequence "TODO" "LATER" "DONE"))
+         org-startup-folded t
+         org-hide-emphasis-markers t))
+
+(use-package org-superstar
+  :hook (org-mode . org-superstar-mode)
+  :config (setq org-superstar-headline-bullets-list '("◉" "○" "✸" "✿")))
+
+(use-package org-evil
+  :after (evil org)
+  :config
+  (org-evil-mode 1))
+
+(use-package company
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package copilot
+  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :config (global-copilot-mode)
+          (with-eval-after-load 'company
+            ;; disable inline previews
+            (delq 'company-preview-if-just-one-frontend company-frontends))
+            
+          (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+          (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))
+
+(use-package fountain-mode
+  :mode "\\.fountain\\'")
+
+(use-package which-key
+  :config
+  (which-key-mode))
