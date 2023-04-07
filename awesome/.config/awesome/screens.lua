4f3c546ff7bc07f83703fcd4993e01948bb32b2c
tarneaux <tarneo@tarneo.fr>
2023-04-07 18:50:05 +0200

    Remove awesomewm spiral layout
    because I don't use it

--- awesome/.config/awesome/screens.lua
+++ awesome/.config/awesome/screens.lua
@@ -41,7 +41,7 @@ awful.layout.layouts = {
     -- awful.layout.suit.tile.top,
     awful.layout.suit.fair,
     -- awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
+    -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
     -- awful.layout.suit.max,
     -- awful.layout.suit.max.fullscreen,
