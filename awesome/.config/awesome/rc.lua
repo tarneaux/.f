0e32030f6dc594577a17437ef08b6c2af5a899f8
tarneo <tarneo@tarneo.fr>
2023-04-25 11:02:39 +0200

    Disable emacs daemon and update keys (awesomewm)

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -59,7 +59,7 @@ awful.spawn.with_shell("mpd")
 -- Emacs: I use emacs as my secondary editor (for screenplays)
 -- Orgmode is also configured in neovim, so I only rarely use emacs.
 -- Running it in the backgroud allows it to start faster when I need it.
-awful.spawn.with_shell("emacs --daemon -q --load ~/.config/emacs/init.el")
+-- awful.spawn.with_shell("emacs --daemon")
 
 -- Unison sync script: syncs files with my server.
 awful.spawn.with_shell("pgrep unison-sync || ~/.config/scripts/unison-sync")
