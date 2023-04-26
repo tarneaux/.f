5e22a24735d9b32c515a8599f77b10ad19077221
tarneo <tarneo@tarneo.fr>
2023-04-26 11:19:00 +0200

    Make media keys work with media instead of mpd

--- awesome/.config/awesome/keys.lua
+++ awesome/.config/awesome/keys.lua
@@ -58,13 +58,13 @@ local globalkeys = gears.table.join(
               awful.spawn.with_shell("pactl set-sink-mute 0 toggle")
               end),
     awful.key({}, "XF86AudioNext", function ()
-              awful.spawn.with_shell("mpc next")
+              awful.spawn.with_shell("playerctl next")
               end),
     awful.key({}, "XF86AudioPrev", function ()
-              awful.spawn.with_shell("mpc prev")
+              awful.spawn.with_shell("playerctl previous")
               end),
     awful.key({}, "XF86AudioPlay", function ()
-              awful.spawn.with_shell("mpc toggle")
+              awful.spawn.with_shell("playerctl play-pause")
               end),
 
     -- Focus (colemak hjkl=neio)
