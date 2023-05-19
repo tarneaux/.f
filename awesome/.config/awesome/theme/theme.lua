2456c8d70127c2e523dba1deec8fc1223d7a4d70
tarneo <tarneo@renn.es>
2023-05-19 11:41:00 +0200

    Change awesomewm wallpaper

--- awesome/.config/awesome/theme/theme.lua
+++ awesome/.config/awesome/theme/theme.lua
@@ -71,9 +71,9 @@ theme.tasklist_fg_normal = theme.color14
 theme.titlebar_fg_focus = theme.color15
 theme.titlebar_fg_normal = theme.color14
 
--- theme.wallpaper = "~/.config/awesome/theme/walls/railway.jpg"
+theme.wallpaper = "~/.config/awesome/theme/walls/railway.jpg"
 -- theme.wallpaper = "~/.config/awesome/theme/walls/calvin.png"
-theme.wallpaper = "~/.config/awesome/theme/walls/mountain-night-3.jpg"
+-- theme.wallpaper = "~/.config/awesome/theme/walls/mountain-night-3.jpg"
 -- theme.wallpaper_color = "#1d2021"
 -- theme.wallpaper_folder = "~/.config/awesome/theme/walls/"
 
