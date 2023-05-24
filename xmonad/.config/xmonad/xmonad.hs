3fecd727b0b99662b307dc82cd2e2f1709f9804f
tarneo <tarneo@renn.es>
2023-05-24 18:26:36 +0200

    A few tweaks to the xmonad config

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -8,6 +8,8 @@ import Language.LSP.Types.Lens (HasWindow(window))
 import XMonad.Layout.Spacing
 import XMonad.Util.SpawnOnce
 import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.Minimize
+import XMonad.Actions.Minimize
 
 myTerminal = "alacritty"
 myBrowser = "qutebrowser"
@@ -19,7 +21,7 @@ myNormalBorderColor = "#ebdbb2"
 myFocusedBorderColor = "#d65d0e"
 
 -- Define layouts
-myLayoutHook = spacingRaw False (Border myGapWidth myGapWidth myGapWidth myGapWidth) True (Border myGapWidth myGapWidth myGapWidth myGapWidth) True $
+myLayoutHook = minimize $ spacingRaw False (Border myGapWidth myGapWidth myGapWidth myGapWidth) True (Border myGapWidth myGapWidth myGapWidth myGapWidth) True $
     Tall 1 (3/100) (1/2) ||| Full
 
 myStartupHook = do
@@ -28,6 +30,8 @@ myStartupHook = do
     spawn "xset r rate 300 50"
     spawn "xsetroot -cursor_name left_ptr"
     spawn "wg-quick up vpn"
+    -- Dunst is a notification daemon
+    spawnOnce "dunst"
 
 myKeys = 
     [ ("M-S-q", io exitSuccess)
@@ -50,11 +54,15 @@ myKeys =
     , ("M-S-n", sendMessage Shrink)
     , ("M-S-o", sendMessage Expand)
     -- Toggle floating
-    , ("M-f", withFocused $ windows . W.sink)
+    , ("M-c", withFocused $ windows . W.sink)
     -- Move focus to the master window
     , ("M-h", windows W.focusMaster)
     -- Swap the focused window and the master window
     , ("M-S-h", windows W.swapMaster)
+    -- Minimize window
+    , ("M-l", withFocused minimizeWindow)
+    -- Restore window
+    , ("M-u", withLastMinimized maximizeWindowAndFocus)
     -- Super + a,r,s,t,d to move to workspaces 1,2,3,4,5
     , ("M-a", windows $ W.greedyView "1")
     , ("M-r", windows $ W.greedyView "2")
@@ -71,6 +79,19 @@ myKeys =
     , ("M-,", sendMessage NextLayout)
     -- DMscripts
     , ("M-m", spawn "bash ~/.config/dmscripts/main.sh")
+    -- Volume
+    , ("<XF86AudioRaiseVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ +2%")
+    , ("<XF86AudioLowerVolume>", spawn "pactl set-sink-volume @DEFAULT_SINK@ -2%")
+    , ("<XF86AudioMute>", spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
+    -- Brightness for my laptop
+    , ("<XF86MonBrightnessUp>", spawn "math '$(cat /sys/class/backlight/intel_backlight/brightness)+500' | sudo tee /sys/class/backlight/intel_backlight/brightness")
+    , ("<XF86MonBrightnessDown>", spawn "math '$(cat /sys/class/backlight/intel_backlight/brightness)-500' | sudo tee /sys/class/backlight/intel_backlight/brightness")
+    -- Screenshot area/window with maim and save to clipboard
+    , ("<Print>", spawn "maim -s --hidecursor | xclip -selection clipboard -t image/png")
+    -- Media keys => playerctl
+    , ("<XF86AudioPlay>", spawn "playerctl play-pause")
+    , ("<XF86AudioNext>", spawn "playerctl next")
+    , ("<XF86AudioPrev>", spawn "playerctl previous")
     ]
 
 main :: IO ()
