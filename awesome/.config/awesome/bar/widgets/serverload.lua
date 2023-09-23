ffdb4cc920650764f3b4bfbbccd273d0722a3e97
tarneo <tarneo@tarneo.fr>
2023-09-23 12:45:27 +0200

    aWM server load widget: display DOWN in red

--- awesome/.config/awesome/bar/widgets/serverload.lua
+++ awesome/.config/awesome/bar/widgets/serverload.lua
@@ -9,7 +9,7 @@ local widget = wibox.widget.textbox()
 local function daemon ()
     awful.spawn.easy_async('zsh -c \'ssh ' .. Server .. ' "uptime" 2> /dev/null | cut -d "," -f 3- | cut -d ":" -f 2- | sed "s/^[ \t]*//"\'', function(stdout)
         if stdout == '' then
-            widget:set_text(icon .. "down")
+            widget:set_markup('<span color="#ff0000">' .. icon .. 'DOWN</span>')
             return false
         else
             widget:set_text(icon .. stdout)
