cc25ca14475fffd199066aa7ab464ed004efa46d
tarneo <tarneo@tarneo.fr>
2023-11-21 14:34:23 +0100

    Remove unneeded dependencies and update installer

--- install-packages.sh
+++ install-packages.sh
@@ -1,18 +1,23 @@
 #! /bin/bash
 set -e
 
-packages=$(grep -v ^# packages.txt | grep -v ^$)
+packages=$(sed 's/#.*$//' packages.txt | tr '\n' ' ' | sed -E "s/ +/ /g" | sed 's/^ //g')
 
-for package in $packages; do
-    echo "Installing $package"
-    yay -S --needed "$package" --noconfirm
-done
+echo "Installing packages: $packages"
 
-# Uninstall dmenu from arch repo
-sudo pacman -R dmenu
+# Installing all packages...
 
-# Install dmenu build
-cd /tmp
-git clone https://github.com/tarneaux/dmenu
-cd dmenu
-make && sudo make install
+yay -S --needed $packages
+
+# Uninstall dmenu from arch repo if present
+if pacman -Qi dmenu &> /dev/null; then
+    sudo pacman -Rsn dmenu
+fi
+
+# Install dmenu build if not already installed
+if ! type dmenu &> /dev/null; then
+    cd /tmp
+    git clone https://github.com/tarneaux/dmenu
+    cd dmenu
+    make && sudo make install
+fi
